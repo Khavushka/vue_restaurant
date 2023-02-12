1d879392c76edd1c8582b18c643352420e0b31e3
Khavushka <khavushka@gmail.com>
2023-02-12 21:01:03 +0100

    Adding Tailwind to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap"
 import router from './routers'
 
 createApp(App).use(router).mount('#app')
